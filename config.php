21027d3f38994661bb73605b0af213fd86c17e76
Dave Menconi <davemenc@gmail.com>
2016-05-25 17:51:59 -0700

    First commit; minimum features in place.

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$dbname="tunatest";
+$dbuser="root";
+$dbpass="";
+$dbhost="127.0.0.1";
+$appnum=20;
+$appword="HgD2vKHdsku";
+$appcookie = $dbname."login";
+$appexpiry = 2200000;
+$pagetitle = "Tuna Music Manager";
+
+$lastmodified = "5/21/16";
+
+?>
